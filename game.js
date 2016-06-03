23fadc921249c124497ae2f02f2b8c463384d3a7
Эльза Достоевская <etopsdos@gmail.com>
2016-06-03 21:07:15 +0400

    8
    let go

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ game =
 	{
 		load: function ()
 		{
-			canvas = window.document.createElement ('canvas');
+			let canvas = window.document.createElement ('canvas');
 			canvas.context = canvas.getContext ('2d');
 
 			canvas.resize = function ()
@@ -13,10 +13,16 @@ game =
 				canvas.width = window.innerWidth;
 			}
 
+			canvas.update = function (event)
+			{
+				switch (event.type)
+				{
+					case 'resize': canvas.resize (); break;
+				}
+			}
+
 			delete game.canvas;
 			game.canvas = canvas;
-
-			game.object.canvas = canvas;
 
 			canvas.resize ();
 			window.document.body.appendChild (canvas);
@@ -27,22 +33,27 @@ game =
 	{
 		button: function (b)
 		{
-			button = b || {};
+			let button = b || {};
 			button.h = button.h || 0.5;
 			button.id = button.id || Object.keys (game.object).length;
 			button.w = button.w || 0.5;
 			button.x = button.x || 0.5;
 			button.y = button.y || 0.5;
 
-			button.click = function ()
+			button.draw = function ()
+			{
+				let o = game.get.metric (button);
+				game.canvas.context.fillRect (o.x, o.y, o.w, o.h);
+			}
+
+			button.mousedown = function ()
 			{
 				console.log ('click');
 			};
 
-			button.draw = function ()
+			button.resize = function ()
 			{
-				o = game.get.metric (button);
-				game.canvas.context.fillRect (o.x, o.y, o.w, o.h);
+				button.draw ();
 			}
 
 			game.object[button.id] = button;
@@ -54,7 +65,7 @@ game =
 	{
 		metric: function (o)
 		{
-			object = {};
+			let object = {};
 			object.h = (o.hk) ? o.hk * o.w * game.canvas.width : o.h * game.canvas.height;
 			object.w = (o.wk) ? o.wk * o.h * game.canvas.height : o.w * game.canvas.width;
 			object.x = (o.xk) ? o.x * game.canvas.width - o.xk * object.w : o.x * game.canvas.width;
@@ -67,31 +78,40 @@ game =
 	{
 		game.window.load ();
 		game.canvas.load ();
+		game.create.button ({ h: 0.1, wk: 1, x: 0.7, xk: 0.5, y: 0.5, yk: 0.5 });
+		game.create.button ({ h: 0.1, wk: 1, x: 0.3, xk: 0.5, y: 0.5, yk: 0.5 });
 		game.create.button ({ h: 0.1, wk: 1, x: 0.5, xk: 0.5, y: 0.5, yk: 0.5 });
 	},
 
-	object: {},
-
-	update: function (event)
+	object:
 	{
-		for (id in game.object)
+		update: function (event)
 		{
-			for (method in game.object[id])
+			for (let id in game.object)
 			{
-				if (method == event.type)
+				for (let method in game.object[id])
 				{
-					game.object[id][method] ();
-					return;
+					if (method == event.type)
+					{
+						game.object[id][method] ();
+					}
 				}
 			}
 		}
+	},
+
+	update: function (event)
+	{
+		game.canvas.update (event);
+		game.object.update (event);
 	},
 
 	window:
 	{
 		load: function ()
 		{
-			window.onclick = game.update;
+			window.onmousedown = game.update;
+			window.onmouseup = game.update;
 			window.onload = game.update;
 			window.onresize = game.update;
 		}
